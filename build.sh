e3390ec40e32d526b2df3fbe632cbc43b97b21ed
Ilan Kasimov <ilan.kasimov@gmail.com>
2025-11-17 10:32:38 +0200

    10.11.3

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
+#/bin/bash
+
+JF_VER=10.11.3   # 17/11/2025
+
+./download_artifacts.sh $JF_VER
+
 docker build --progress=plain --no-cache \
   --build-arg PACKAGE_ARCH=armhf \
   --build-arg DOTNET_ARCH=arm \
   --build-arg IMAGE_ARCH=arm32v7 \
   --build-arg TARGET_ARCH=arm/v7 \
-  --build-arg JELLYFIN_VERSION=10.11.2 \
+  --build-arg JELLYFIN_VERSION=$JF_VER \
   --build-arg CONFIG=Release \
   --build-arg DOTNET_VERSION=9.0 \
   --build-arg NODEJS_VERSION=20 \
   --file /mnt/usb/Git/jellyfin-arm32/Dockerfile \
-  --tag jellyfin/jellyfin:10.11.2-arm32v7 /mnt/usb/Git/jellyfin-arm32
+  --tag jellyfin/jellyfin:$JF_VER-arm32v7 /mnt/usb/Git/jellyfin-arm32
 
--- download_artifacts.sh
+++ download_artifacts.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
+
 set -e  # Exit immediately if a command fails
 
+# Check if version argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <JELLYFIN_VERSION> (ex: 10.11.3)"
+    exit 1
+fi
+
 # Jellyfin version
-JELLYFIN_VERSION="v10.11.2"
+JF_VER="$1"
+JELLYFIN_VERSION="v$JF_VER"
+
+echo "Using Jellyfin version: $JELLYFIN_VERSION"
+
 
 # Define URLs and target directories using the version variable
 WEB_URL="https://github.com/jellyfin/jellyfin-web/archive/refs/tags/${JELLYFIN_VERSION}.tar.gz"
 WEB_DIR="jellyfin-web"
+rm -rf $WEB_DIR
 
 SERVER_URL="https://github.com/jellyfin/jellyfin/archive/refs/tags/${JELLYFIN_VERSION}.tar.gz"
 SERVER_DIR="jellyfin-server"
+rm -rf $SERVER_DIR
 
 # Download and extract Jellyfin Web
 echo "Downloading Jellyfin Web..."
@@ -28,3 +41,5 @@ tar -xzf "${JELLYFIN_VERSION}-server.tar.gz" --strip-components=1 -C "$SERVER_DI
 rm "${JELLYFIN_VERSION}-server.tar.gz"
 
 echo "Done!"
+
+exit 0
